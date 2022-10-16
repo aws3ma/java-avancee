e86a4e62d6b7de195ed4b6a02e401cf01d5b57a8
Oussema Trabelsi <79041057+aws3ma@users.noreply.github.com>
2022-10-16 10:45:13 +0100

    test jdbc

---
+++ cours/ex3/Test.java
@@ -0,0 +1,19 @@
+import java.sql.SQLException;
+
+class Test {
+    public static void main(String[] args) throws SQLException {
+        // Connexion cnx = new Connexion();
+        // cnx.getInstance();
+        // Connexion cnx = new Connexion();
+        // Connection cnt = cnx.getInstance();
+        // System.out.println(cnt);
+        Traitement t = new Traitement();
+        // Produit p = new Produit("P3", "Produit 3", "Produit trois", 100f);
+        t.selectAllProduit();
+        // if(t.createProduit(p)) System.out.println("created");
+        // if(t.updateProduct(p)) System.out.println("updated");
+        // if(t.deleteProduct("P3")) System.out.println("delated");
+
+        // System.out.println(t.selectProduitByRef("P3"));
+    }
+}
