c1329f4fff17cd19a0cb9bcf3ecbcd4f0dedc14d
Oussema Trabelsi <79041057+aws3ma@users.noreply.github.com>
2022-11-06 18:09:03 +0100

    minimise code

--- cours/excours/Ex.java
+++ cours/excours/Ex.java
@@ -17,18 +17,13 @@ public class Ex{
         f.setLayout(new GridLayout(3,1));
 
         Panel p = new Panel();
-        p.add("b1", new Button("B1"));
-        p.add("b2", new Button("B2"));
-        p.add("b3", new Button("B3"));
-        p.add("b4", new Button("B4"));
-        f.add(p);
-
         Panel p2 = new Panel();
         p2.setLayout(new GridLayout(2,2));
-        p2.add("GR1", new Button("GR1"));
-        p2.add("GR2", new Button("GR2"));
-        p2.add("GR3", new Button("GR3"));
-        p2.add("GR4", new Button("GR4"));
+        for (int i = 1; i < 5; i++) {
+            p.add("b"+i, new Button("B"+i));
+            p2.add("GR"+i, new Button("GR"+i));
+        }
+        f.add(p);
         f.add(p2);
 
         Panel p3 = new Panel();
